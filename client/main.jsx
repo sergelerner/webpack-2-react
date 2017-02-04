5d15132b87f6c04585992a05313abd7ce1d759d0
Serge Lerner <serge@yodas.com>
2017-02-04 12:52:37 +0200

    Add main.jsx

---
+++ client/main.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router/es6';
+
+const root = document.createElement('root');
+document.body.appendChild(root);
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <h1>Hello World!!!</h1>
+      </main>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+  </Router>
+  ,
+  root
+);
