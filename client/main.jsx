6c6569a552d561d472b772145c3e51a0023e0dcf
Serge Lerner <serge@yodas.com>
2018-02-12 16:20:17 +0200

    Add support for decorators

--- client/main.jsx
+++ client/main.jsx
@@ -11,6 +11,17 @@ document.body.appendChild(root);
 
 const history = createBrowserHistory();
 
+const magic = () => WrappedComponent => class Magic extends Component {
+  render() {
+    return (
+      <div className="magic">
+        <WrappedComponent {...this.props}/>
+      </div>
+    );
+  }
+};
+
+@magic()
 class App extends Component {
   render() {
     return (
